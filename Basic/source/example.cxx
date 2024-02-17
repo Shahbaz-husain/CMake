2c8c9b04d3edaa2a4fbeca1e4d5b06c145ee61a8
Mohd Shahbaz Husain <hussainmshahbaz67@gmail.com>
2024-02-17 17:57:48 +0530

    Initial commit

---
+++ Basic/source/example.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <concepts>
+
+using namespace std;
+
+template <typename T>
+void printInteger(T value) {
+    cout << value << endl;
+}
+
+int main() {
+    printInteger(42);
+    //printInteger("Hello"); // Error: non-integral type
+    return 0;
+}
